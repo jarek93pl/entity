4cdbace387082447616013f81527db7c28da3062
Jarosław Gosławski <jarek93pl@hotmail.com>
2025-04-01 14:15:40 +0200

    Add genertated controler

--- EntityWithMvc/Data/ApplicationDbContext.cs
+++ EntityWithMvc/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EntityWithMvc.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntityWithMvc.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Ticket> Tickets { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
---
+++ EntityWithMvc/Models/Ticket.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityWithMvc.Models
+{
+    public class Ticket
+    {
+        [MaxLength(),Column(TypeName ="varchar(15)")]
+        public string MyProperty { get; set; }
+
+        public int Id { get; set; }
+    }
+}
